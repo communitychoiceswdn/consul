48be3d6f0a236ba174b3c7a41907cdc4daffefa7
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2020-12-02 18:51:38 +0000

    Add metadata parser instructions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,6 +241,21 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+
+  #
+  require 'omniauth'
+  idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+  idp_metadata = idp_metadata_parser.parse_remote_to_hash(Rails.application.secrets.saml_idp_metadata)
+
+  # or, if you have the metadata in a String:
+  # idp_metadata = idp_metadata_parser.parse_to_hash(idp_metadata_xml)
+
+  use OmniAuth::Strategies::SAML,
+    idp_metadata.merge(
+      :assertion_consumer_service_url => "consumer_service_url",
+      :issuer                         => "issuer"
+    )
+
   config.omniauth :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret
   config.omniauth :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret, scope: "email", info_fields: "email,name,verified"
   config.omniauth :google_oauth2, Rails.application.secrets.google_oauth2_key, Rails.application.secrets.google_oauth2_secret
