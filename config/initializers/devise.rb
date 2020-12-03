9c0300235c2f46052d8dc120a5bf4eacb41986c8
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2020-12-03 11:07:20 +0000

    Add metadata parser instructions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,7 +264,6 @@ Devise.setup do |config|
                   client_options: { site: Rails.application.secrets.wordpress_oauth2_site }
   config.omniauth :saml,
                   idp_cert_fingerprint: Rails.application.secrets.saml_idp_cert_fingerprint,
-                  idp_sso_target_url: Rails.application.secrets.saml_idp_sso_target_url,
                   certificate: Rails.application.secrets.saml_certificate,
                   private_key: Rails.application.secrets.saml_private_key,
                   request_attributes: [
