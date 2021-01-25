c2a2127981cad238f66d331815017580cc179d5f
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2021-01-25 11:10:32 +0000

    Comment out lacode verification for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -128,10 +128,10 @@ class User < ApplicationRecord
   def self.first_or_initialize_for_oauth(auth)
     oauth_email           = auth.info.email
     oauth_email_confirmed = oauth_email.present? && (auth.info.verified || auth.info.verified_email)
-  # ouath_lacode              = "230"
+    ouath_lacode              = "230"
   # ouath_lacode              = auth.extra.raw_info.lacode
-  # ouath_lacode_ref          = "230"
-  # ouath_lacode_confirmed    = (oauth_lacode == oauth_lacode_ref ? true : false)
+    ouath_lacode_ref          = "230"
+    ouath_lacode_confirmed    = (oauth_lacode == oauth_lacode_ref ? true : false)
   # ouath_lacode_confirmed    = true
     oauth_user            = User.find_by(email: oauth_email) if oauth_email_confirmed
 
@@ -142,7 +142,7 @@ class User < ApplicationRecord
       password: Devise.friendly_token[0, 20],
       terms_of_service: "1",
       confirmed_at: oauth_email_confirmed ? DateTime.current : nil,
-   #  verified_at: oauth_lacode_confirmed ? DateTime.current : nil
+      verified_at: oauth_lacode_confirmed ? DateTime.current : nil
     )
   end
 
