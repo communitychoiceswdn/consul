e03ebaf8344a40e9bf430d87b2c5d67bc9849ae7
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2021-03-11 08:58:53 +0000

    Add lacode to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -131,7 +131,7 @@ class User < ApplicationRecord
     oauth_lacode              = auth.info.description
     #oauth_date_of_birth = auth.extra.raw_info.all.dig("urn:oid:0.9.2342.19200300.100.1.8", 0).to_s
     #oauth_gender = auth.extra.raw_info.all.dig("urn:oid:0.9.2342.19200300.100.1.9", 0).to_s
-    oauth_lacode_ref          = Rails.application.secrets.council_lacode
+    oauth_lacode_ref          = "9064"
     oauth_lacode_confirmed    = oauth_lacode == oauth_lacode_ref
     oauth_user            = User.find_by(email: oauth_email) if oauth_email_confirmed
 
