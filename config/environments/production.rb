c65f03001822e93c2806ac93bb5ea17f49caeaee
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2021-03-16 19:58:37 +0000

    Remove CSRF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable request forgery protection in production environment.
-  config.action_controller.allow_forgery_protection = true
+  config.action_controller.allow_forgery_protection = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
